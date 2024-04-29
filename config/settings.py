91a4af518997ca8404e60bb634de9badb4f85bb7
Deepak <ootaladeepak128@gmail.com>
2024-04-29 19:07:48 +0530

    environment correction

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t)(4948=-*n)e7*$%!j^x9i0oghb-6i6z6y0$z-*bl0*ax+#qt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-RENDER_EXTERNAL_HOSTNAME = env("RENDER_EXTERNAL_HOSTNAME", default=None)
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME", default=None)
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
