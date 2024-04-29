206393bd47b95e97e93691a479abf48ffe3833d3
Deepak <ootaladeepak128@gmail.com>
2024-04-29 18:20:00 +0530

    render ready

--- config/settings.py
+++ config/settings.py
@@ -156,7 +156,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
